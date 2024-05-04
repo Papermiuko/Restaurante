bb549fd0193c80a79ce2c367e55c3711c1431552
fonse <fonsecaperez.oscar@gmail.com>
2024-05-03 19:04:16 -0500

    Prueba3 de settings.py

--- Restaurante_Sabor_Express/settings.py
+++ Restaurante_Sabor_Express/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # Configuración de archivos estáticos
-STATIC_URL = '/staticfiles/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Restaurante_Sabor_Express', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
